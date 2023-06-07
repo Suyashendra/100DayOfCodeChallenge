f434da346e7190b64548477ac0407aa33c518ddb
Suyashendra Rathore <67049212+Suyashendra@users.noreply.github.com>
2023-06-07 18:58:18 +0530

    Day3

--- Day3.py
+++ Day3.py
@@ -48,9 +48,9 @@ z = float("3")   # z will be 3.0
 w = float("4.2") # w will be 4.2
 
 # Strings:
-x = str("s1") # wil be 's1' 
-y = str(2)    # wil be '2'
-z = str(3.0)  # wil be '3.0'
+x = str("s1") # x will be 's1' 
+y = str(2)    # y will be '2'
+z = str(3.0)  # z will be '3.0'
 
 # Example3: Addition of string and integer
 n_str = '12'
